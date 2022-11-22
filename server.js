143de3870e8dffebf113233eebcf8c87fea124ed
shreyansh.gohil <shreyansh.gohil@lucentinnovation.co.in>
2022-11-22 22:18:41 +0530

    comment removed

--- server.js
+++ server.js
@@ -28,21 +28,5 @@ const fetchInitialData = async () => {
   const tokenData = await getAcessTokenHandler();
   const routeData = await getRouteTimeTableHandler(tokenData);
   setRouteDetailsSchemaToMongo(routeData);
-  // let options = {
-  //   host: "jsonplaceholder.typicode.com",
-  //   port: 443,
-  //   path: "/posts",
-  //   method: "GET",
-  // };
-
-  // // do the GET request
-  // let reqGet = https.request(options, function (res) {
-  //   res.setEncoding("utf-8");
-  //   res.on("data", function (d) {
-  //     console.log(d);
-  //   });
-  // });
-
-  // reqGet.end();
 };
 fetchInitialData();
