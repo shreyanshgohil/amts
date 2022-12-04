0fb90b243a07a2c9eb05f93c977632c28d0667f5
shreyanshgohil <shreyanshgohil2510@gmail.com>
2022-12-04 23:37:26 +0530

    basic done

--- gql/resolvers/busDetailsResolvers.js
+++ gql/resolvers/busDetailsResolvers.js
@@ -1,41 +1,46 @@
 import RouteDetails from "../../models/RouteDetails.js";
 const busDetailsResolvers = {
   Query: {
+    // Fetch the data from the api and retuns to the user
     getBusDetails: async (parent, args, context, info) => {
-      const { startLocation, endLocation, busTime } = args.busDetails;
-      const busDetailsWithRoute = await RouteDetails.aggregate([
-        {
-          $match: {
-            startName: { $regex: startLocation, $options: "i" },
-            endName: { $regex: endLocation, $options: "i" },
+      try {
+        const { startLocation, endLocation, busTime } = args.busDetails;
+        const busDetailsWithRoute = await RouteDetails.aggregate([
+          {
+            $match: {
+              startName: { $regex: startLocation, $options: "i" },
+              endName: { $regex: endLocation, $options: "i" },
+            },
           },
-        },
-        {
-          $lookup: {
-            from: "busdetails",
-            as: "data",
-            foreignField: "routeCode",
-            localField: "routeCode",
+          {
+            $lookup: {
+              from: "busdetails",
+              as: "data",
+              foreignField: "routeCode",
+              localField: "routeCode",
+            },
           },
-        },
-        {
-          $project: {
-            _id: 0,
-            filterdBuses: {
-              $filter: {
-                input: "$data",
-                as: "sc",
-                cond: { $eq: ["$$sc.startTime", busTime] },
+          {
+            $project: {
+              _id: 0,
+              filterdBuses: {
+                $filter: {
+                  input: "$data",
+                  as: "sc",
+                  cond: { $eq: ["$$sc.startTime", busTime] },
+                },
               },
             },
           },
-        },
-        {
-          $match: { $expr: { $gte: [{ $size: "$filterdBuses" }, 1] } },
-        },
-      ]);
-
-      return busDetailsWithRoute;
+          {
+            $match: { $expr: { $gte: [{ $size: "$filterdBuses" }, 1] } },
+          },
+        ]);
+        return busDetailsWithRoute;
+        
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
